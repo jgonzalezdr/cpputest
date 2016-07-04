b16435474de7539dc02f85d5b2923cf8b2ad0028
Jesús González <jgonzalez@gdr-sistemas.com>
2016-07-04 22:05:39 +0200

    Added the actual calls queue to the DOS/Watcom build system.

--- platforms/Dos/sources.mk
+++ platforms/Dos/sources.mk
@@ -34,6 +34,7 @@ CPPUX_OBJECTS := \
   $(CPPUTEST_HOME)/src/CppUTestExt/OrderedTest.o \
   $(CPPUTEST_HOME)/src/CppUTestExt/MemoryReportFormatter.o \
   $(CPPUTEST_HOME)/src/CppUTestExt/MockExpectedCallsList.o \
+  $(CPPUTEST_HOME)/src/CppUTestExt/MockActualCallsQueue.o \
   $(CPPUTEST_HOME)/src/CppUTestExt/MockSupport.o
 
 CPPU1_OBJECTS := \
@@ -139,6 +140,7 @@ CPPUX6_OBJECTS := \
   $(CPPUTEST_HOME)/tests/CppUTestExt/AllTests.o \
   $(CPPUTEST_HOME)/tests/CppUTestExt/MockFailureReporterForTest.o \
   $(CPPUTEST_HOME)/tests/CppUTestExt/ExpectedFunctionsListTest.o \
+  $(CPPUTEST_HOME)/tests/CppUTestExt/ActualCallsQueueTest.o \
   $(CPPUTEST_HOME)/tests/CppUTestExt/MockCallTest.o
 
 CPPUX7_OBJECTS := \
